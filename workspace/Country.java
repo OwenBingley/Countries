f27f9a1eda05041292b3c6186ecfca2671a95a37
OwenBingley <owenbingley10@gmail.com>
2026-01-08 16:02:36 +0000

    save 1

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,7 +1,10 @@
 public class Country
 {
   // add private instance variables for the name, capital, language, and image file.
-
+private String name;
+private String capital;
+private String language;
+private String imageFile;
   // add constructors
 
   // Write accessor/get methods for each instance variable that returns it.
